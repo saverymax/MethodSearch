31cb80e1721ec4f4ba1ceb185091f4102259b1ec
Savery <saveryme@nih.gov>
2018-08-30 10:23:30 -0400

    packaging materials

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="MethodSearch",
+    version="0.0.1",
+    author="Max Savery",
+    author_email="max.savery@nih.gov",
+    description="classification of method sections",
+    #long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saverymax/methodsearch",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
